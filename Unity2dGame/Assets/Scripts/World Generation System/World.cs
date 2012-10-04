08d9c068ea255fa01cbf6e8cb0b1bdd335071f11
Jonas Kugelmann <>
2012-10-04 23:50:38 +0200

    Can Generate circles but need to change radius of capsules or radius of the circles to get complete circles with even spacing or find another way for evenspacing

--- Unity2dGame/Assets/Scripts/World Generation System/World.cs
+++ Unity2dGame/Assets/Scripts/World Generation System/World.cs
@@ -5,19 +5,43 @@ using Random = UnityEngine.Random;
 public class World : MonoBehaviour
 {
     
-    public GameObject prefab;
+    public CapsuleCollider Prefab;
     
     // Use this for initialization
     private void Start()
     {
+        CreateCircleOfPrefabs(2);
+        CreateCircleOfPrefabs(4); 
+        CreateCircleOfPrefabs(6);
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-
+        
     }
 
+    private void CreateCircleOfPrefabs(float radius)
+    {
+        var circum = 2*Mathf.PI*radius;
+        var countOfPrefabs = (circum/(Prefab.radius*2));
+        var startPoint = RandomOnUnitCircle()*radius;
+        Debug.Log("Count Needed: "+ countOfPrefabs);
+        var stepAngle = (360 / countOfPrefabs);
+        Debug.Log("StepAngle: "+ stepAngle);
+        var quat = Quaternion.AngleAxis(stepAngle, Vector3.forward);
+        for (int i = 0; i < (int)countOfPrefabs; i++)
+        {
+            Instantiate(Prefab, startPoint, Prefab.transform.rotation);
+            startPoint = quat * startPoint;
+        }
+    }
 
+    private Vector2 RandomOnUnitCircle()
+    {
+        var randomAngle =Random.Range(0, 360);
+        var quat = Quaternion.AngleAxis(randomAngle, Vector3.forward);
+        return quat * Vector2.up;
+    }
 }
